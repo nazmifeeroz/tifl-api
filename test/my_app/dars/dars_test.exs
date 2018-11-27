b2437b27e4f29ce0fca4fcb47179b5e0afb8f20a
Nazmi <nazmifeeroz@hotmail.com>
2018-11-27 20:54:59 +0800

    create post test done

--- test/my_app/dars/dars_test.exs
+++ test/my_app/dars/dars_test.exs
@@ -5,15 +5,23 @@ defmodule MyApp.DarsTest do
   @endpoint MyAppWeb.Endpoint
 
   alias MyApp.Dars
+  alias MyApp.Auth
+  alias MyApp.Auth.User
 
   describe "dars" do
     
-    @valid_attrs %{title: "some title", body: "some body"}
+    @post_attrs %{title: "some title", body: "some body"}
+    @user_attrs %{email: "some email", is_active: true, password: "some password", role: "admin"}
+
+    defp auth_user(conn, user) do
+      token = MyApp.Auth.sign(%{id: user.id})
+      put_req_header(conn, "authorization", "Bearer #{token}")
+    end
 
     def post_fixture(attrs \\ %{}) do
       {:ok, post} =
         attrs
-        |> Enum.into(@valid_attrs)
+        |> Enum.into(@post_attrs)
         |> Dars.create_post()
 
       post
@@ -30,7 +38,33 @@ defmodule MyApp.DarsTest do
     test "list_posts returns all post" do
       post = post_fixture()
       assert Dars.list_posts() == [post]
-      # assert Dars.list_posts() == [%Post{post | body: nil}]
+    end
+
+    @query """
+    mutation ($createPostInput: CreatePostInput!) {
+      createPost(input: $createPostInput) {
+        title
+        body
+      }
+    }
+    """
+    test "create_post" do
+      assert {:ok, %User{} = user} = Auth.create_user(@user_attrs)
+      conn = build_conn() |> auth_user(user)
+
+      conn = post conn, "/api",
+        query: @query,
+        variables: %{"createPostInput" => @post_attrs}
+
+      assert json_response(conn, 200) == %{
+          "data" => %{
+            "createPost" => %{
+              "body" => "some body",
+              "title" => "some title"
+            }
+          }
+        }
+        
     end
 
     test "create post" do
