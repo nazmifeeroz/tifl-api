3347b95a6fe5b29954f0a25f104e6fbaafc44860
Nazmi <nazmifeeroz@hotmail.com>
2018-11-28 08:10:00 +0800

    creating comments

---
+++ lib/my_app/commenting/comment.ex
@@ -0,0 +1,24 @@
+defmodule MyApp.Commenting.Comment do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias MyApp.Commenting.Comment
+
+
+  schema "comments" do
+    field :commented_at, :utc_datetime
+    field :state, :string
+    field :comment, :text
+
+    field :post_id, :id
+    field :user_id, :id
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(comment, attrs) do
+    comment
+    |> cast(attrs, [:user_id, :post_id, :commented_at, :state])
+    |> validate_required([:user_id, :post_id, :commented_at, :state])
+  end
+end
